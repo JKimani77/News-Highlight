efea0eabf4b4b07fee97f0a0714f975fd6c6872e
JKimani77 <jkkimani77@gmail.com>
2020-02-03 12:22:04 +0300

    added a control block with an if statement in index.html for the title

--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'NEWS'
+    title = 'Home - Welcome to The best NEWS HIGHLIGHT Website online'
     return render_template('index.html', title = title)
 
 @app.route('/newsatnine/<int:movie_id>')
