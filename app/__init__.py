e70fb19bbadb3393c487657b15ca2d65d2f2d7b7
JKimani77 <jkkimani77@gmail.com>
2020-02-05 15:00:10 +0300

    made application configurations and created a new folder main

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,27 @@
 from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+bootstrap = Bootstrap()
+
+def create_app(config_name):
 
 # to init the  app
 app = Flask(__name__, instance_relative_config = True)
 
 #to setup the configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+app.config.from_object(config_options[config_name])
 
 
 # Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+bootstrap.init_app(app)
 
 
 from app import views
 from app import errors
+
+return app
 
 
